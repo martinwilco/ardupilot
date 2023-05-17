989e30b5c594ee207477bba218b9b804a665f524
Martin Wilichowski <38102591+martinwilco@users.noreply.github.com>
2023-05-17 17:44:19 +0200

    CDC LUA P14 humidity offset param added

--- ROMFS_custom/scripts/read_and_log_cdc.lua
+++ ROMFS_custom/scripts/read_and_log_cdc.lua
@@ -1,22 +1,23 @@
 --[[
-    This script reads a P14 Rapid humidity sensor on I²C,
+    This script reads a P14 Rapid humidity sensor via CDC AD7745 on I²C,
     reading will be saved to data flash logs and streamed to gcs.
 ]]
 --
 
--- register names
+-- CDC register names
+local CDC_I2C_ADDR = 72; --I²C address 0x48
 local CDC_STATUS = 0x00;
+local CDC_CAP_DATA = 0x01;
+local CDC_VT_DATA = 0x04;
 local CDC_CAP_SETUP = 0x07;
 local CDC_VT_SETUP = 0x08;
 local CDC_EXC_SETUP = 0x09;
 local CDC_CONFIG = 0x0A;
 local CDC_CAP_DAC_A = 0x0B;
 local CDC_CAP_DAC_B = 0x0C;
-local CDC_CAP_DATA = 0x01;
-local CDC_VT_DATA = 0x04;
 
--- load the i2c driver, bus 0
-local sensor = i2c:get_device(0, 72)
+-- load the I²C driver, bus 0
+local sensor = i2c:get_device(0, CDC_I2C_ADDR)
 sensor:set_retries(10)
 
 -- reads three consecutive bytes from a given location
@@ -29,14 +30,23 @@ local function data(addr)
   end
 end
 
+-- parameter for p14 humidity offset, default -106
+local PARAM_TABLE_KEY = 72
+assert(param:add_table(PARAM_TABLE_KEY, "P14_", 30), 'could not add param table')
+assert(param:add_param(PARAM_TABLE_KEY, 1, 'OFFSET', -106), 'could not add param1')
+
+-- get capacitance and convert it to humidity
 local function getcap(addr)
   local cap = data(addr)
-  -- gcs:send_text(6, "CDC: capacitance "..tostring(cap))
   if cap then
-    return cap / 42279.1 - 151.5 - 106
+    local P14_OFFSET = Parameter()
+    P14_OFFSET:init('P14_OFFSET')
+    local p14_offset = P14_OFFSET:get()
+    return cap / 42279.1 - 151.5 + p14_offset
   end
 end
 
+-- get voltage/temperature, return temperature
 local function getvt(addr)
   local vt = data(addr)
   if vt then
@@ -44,20 +54,22 @@ local function getvt(addr)
   end
 end
 
--- initialize cdc
-sensor:write_register(CDC_CAP_SETUP, 0x80)
-sensor:write_register(CDC_VT_SETUP, 0x80)
-sensor:write_register(CDC_EXC_SETUP, 0x1B)
-sensor:write_register(CDC_CONFIG, 0x01)
+-- set TSYS01 Source ID parameter (battery number)
+param:set('TEMP1_SRC', 3)
+param:set('TEMP1_SRC_ID', 1)
+
+-- initialize CDC
+sensor:write_register(CDC_CAP_SETUP, 0x80) --bit 7 CAPEN = 1
+sensor:write_register(CDC_VT_SETUP, 0x80)  --bit 7 VTEN = 1
+sensor:write_register(CDC_EXC_SETUP, 0x1B) --bit 0 EXCLVL0 = 1, bit 1 EXCLVL0 = 1, bit 3 EXCA = 1, 4 EXCB = 1
+sensor:write_register(CDC_CONFIG, 0x01)    --bit 0 MD0 = 1
 sensor:write_register(CDC_CAP_DAC_A, 200)
 sensor:write_register(CDC_CAP_DAC_B, 0x00)
 
 function update()
-  -- check, whether CDC is connected and if so, read status
+  -- check whether CDC is connected and if so, read status
   if sensor:read_registers(0) then
-    -- gcs:send_text(6, "CDC: found at 0x48")
     status = sensor:read_registers(CDC_STATUS)
-    -- gcs:send_text(6,"CDC: status "..tostring(status))
     if not status then
       gcs:send_text(3, "CDC: failed to read status")
       return update, 10000
@@ -67,6 +79,7 @@ function update()
     return update, 10000
   end
 
+  -- show TSYS01 temperature on Mission Planner Quicktab
   local tsys01_temp = battery:get_temperature(0)
   if tsys01_temp then
     gcs:send_named_float('TSYS (°C)', tsys01_temp)
@@ -75,22 +88,24 @@ function update()
   -- status register bit map
   local EXCERR = (status >> 3) & 1
   local RDY = (status >> 2) & 1
-  local RDYVT = (status >> 1) & 1
-  local RDYCAP = (status >> 0) & 1
+  -- following status bits are not needed, temperature and humidity is only read when new data for both is available
+  -- local RDYVT = (status >> 1) & 1
+  -- local RDYCAP = (status >> 0) & 1
 
-  -- report excitation output errors
+  -- report CDC excitation output errors
   if EXCERR == 1 then
     gcs:send_text(3, "CDC: excitation output cannot be driven properly")
     return update, 10000
   end
 
-  if RDY == 0 then -- report capacitance and voltage/temperature conversion status
+  -- check if capacitance and voltage/temperature conversion is finished, read and log data
+  if RDY == 0 then
     local hum = getcap(CDC_CAP_DATA)
     local temp = getvt(CDC_VT_DATA)
-    gcs:send_named_float('P14 (°C)', temp)
-    gcs:send_named_float('P14 (% RH)', hum)
+    gcs:send_named_float('CDC (°C)', temp)
+    gcs:send_named_float('CDC (% RH)', hum)
     if temp and hum then
-      logger:write('P14', 'Humidity,Temperature', 'ff', '-O', '--', hum, temp)
+      logger:write('CDC', 'Humidity,Temperature', 'ff', '-O', '--', hum, temp)
     else
       gcs:send_text(1, "CDC: failed to read data")
     end
