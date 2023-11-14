e51849472bc05fe260b832d90ce67e8b73f8399e
Martin Wilichowski <m.wilichowski@tu-braunschweig.de>
2023-11-14 12:31:47 +0100

    Change FlightGear start position to airfield EDVP

--- Tools/autotest/fg_quad_view_AppImage.sh
+++ Tools/autotest/fg_quad_view_AppImage.sh
@@ -5,20 +5,26 @@ AUTOTESTDIR=$(dirname $0)
 nice /$HOME/Programme/FlightGear.AppImage \
     --native-fdm=socket,in,10,,5503,udp \
     --fdm=external \
-    --aircraft=arducopter \
+    --aircraft=IRIS \
     --fg-aircraft="$AUTOTESTDIR/aircraft" \
-    --airport=YKRY \
+    --lat=52.402791 \
+    --lon=10.230939 \
+    --altitude=78 \
+    --heading=74 \
     --geometry=650x550 \
     --bpp=32 \
+    --max-fps=30 \
+    --timeofday=noon \
     --disable-hud-3d \
     --disable-horizon-effect \
-    --timeofday=noon \
     --disable-sound \
     --disable-fullscreen \
     --disable-random-objects \
     --disable-ai-models \
+    --disable-clouds \
     --fog-disable \
     --disable-specular-highlight \
     --disable-anti-alias-hud \
     --wind=0@0 \
+	--enable-terrasync \
     $*
