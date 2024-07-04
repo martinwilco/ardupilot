22e0e6846ec17a1005453ff6e2860aea95d5b7c6
Martin Wilichowski <38102591+martinwilco@users.noreply.github.com>
2024-07-04 19:24:33 +0200

    HMP LUA add parameter to adjust logging rate

--- ROMFS_custom/scripts/read_and_log_hmp.lua
+++ ROMFS_custom/scripts/read_and_log_hmp.lua
@@ -1,3 +1,34 @@
+--[[
+    This script reads a HMP humidity sensor via UART,
+    reading will be saved to data flash logs and streamed to gcs.
+    The logging rate is adjustable via parameter HMP_LOG_RATE.
+--]]
+
+-- create parameter table
+local PARAM_TABLE_KEY = 73 -- parameter table key must be used by only one script on a particular flight controller, unique index value between 0 and 200
+local PARAM_TABLE_PREFIX = 'HMP_'
+assert(param:add_table(PARAM_TABLE_KEY, PARAM_TABLE_PREFIX, 1),
+  string.format('Could not add param table %s', PARAM_TABLE_PREFIX))
+
+-- add a parameter and bind it to a variable
+local function bind_add_param(name, idx, default_value)
+  assert(param:add_param(PARAM_TABLE_KEY, idx, name, default_value), string.format('Could not add param %s', name))
+  return Parameter(PARAM_TABLE_PREFIX .. name)
+end
+
+--[[
+  // @Param: HMP_LOG_RATE
+  // @DisplayName: HMP logging rate
+  // @Description: Adjust HMP logging rate in ms
+  // @Units: ms
+  // @User: Standard
+--]]
+HMP_LOG_RATE = bind_add_param('LOG_RATE', 1, 300) -- logging rate [ms]
+
+local LOG_RATE = Parameter()
+LOG_RATE:init('HMP_LOG_RATE')
+
+
 local port = serial:find_serial(0)
 
 port:begin(19200)
@@ -18,8 +49,7 @@ function decode_and_write_to_flash(fullmessage)
     count = count + 1
     log_data[count] = tonumber(word)
   end
-  if count == 3 then
-    -- nur wegschreiben, wenn alles da ist. und auch nicht zuviel.
+  if count == 3 then -- only log if everything is there
     logger.write('HMP', 'Temperature,Humidity,Dewpoint', 'fff', 'O-O', '---', table.unpack(log_data))
     gcs:send_named_float('HMP (°C)', log_data[1])
     gcs:send_named_float('HMP (% RH)', log_data[2])
@@ -42,7 +72,7 @@ function spit()
       n_bytes = n_bytes - 1
     end
   end
-  return spit, 300
+  return spit, LOG_RATE:get()
 end
 
-return spit, 300 -- do we need to wait here?
+return spit()
