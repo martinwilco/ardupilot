d9a0d11437c8014575275ad56468d27d99d2a8fb
Martin Wilichowski <38102591+martinwilco@users.noreply.github.com>
2025-04-02 15:55:04 +0200

    Change FlightGear default airport to "Eddesse", enable terrasync

--- Tools/autotest/fg_quad_view.bat
+++ Tools/autotest/fg_quad_view.bat
@@ -5,12 +5,13 @@ echo "Using FlightGear %FGDIR%"
 cd "\Program Files\%FGDIR%\bin"
 
 fgfs ^
-    --native-fdm=socket,in,10,,5503,udp ^
+    --native-fdm=socket,in,30,,5503,udp ^
     --fdm=external ^
     --aircraft=arducopter ^
     --fg-aircraft=%AUTOTESTDIR% ^
-    --airport=KSFO ^
-    --geometry=650x550 ^
+    --lat=52.402791 ^
+	--lon=10.230939 ^
+    --geometry=650x480 ^
     --bpp=32 ^
     --disable-hud-3d ^
     --disable-horizon-effect ^
@@ -22,5 +23,6 @@ fgfs ^
     --fog-disable ^
     --disable-specular-highlight ^
     --disable-anti-alias-hud ^
-    --wind=0@0
+    --wind=0@0 ^
+    --enable-terrasync
 pause
